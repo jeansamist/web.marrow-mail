2cd259ac5c8e60834387251ea0b83b451ab45ea2
BAHA Ephraim <jeansamist@gmail.com>
2026-06-17 03:47:19 +0100

    feat: refactor mail services to use centralized API handler and add axios instance for mail requests

--- services/mail.services.ts
+++ services/mail.services.ts
@@ -1,13 +1,9 @@
 "use server"
-import { POST } from "@/lib/api"
+import { POST } from "@/lib/api-mail"
 import { SetupMailAccountProfileSchema } from "@/schemas/auth.schemas"
-import { Record } from "@/types"
 
 export const setupMailAccountProfile = async (
   data: SetupMailAccountProfileSchema
 ) => {
-  return POST<SetupMailAccountProfileSchema, Record[]>(
-    "/mail/setup-profile",
-    data
-  )
+  return POST<SetupMailAccountProfileSchema, unknown>("/setup-profile", data)
 }
